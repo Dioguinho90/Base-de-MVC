5a87492bc85e1e30dc3918b49f22f1b5e2c9f7dc
Aluno <e023aluno@outlook.com>
2026-09-11 21:31:52 -0300

    Cliente Model js Criado

--- models/ClienteModel.js
+++ models/ClienteModel.js
@@ -1,12 +1,66 @@
 const db = require('../config/db');
 
 const Cliente = {
-    createNewClient: async (nome, cpf) =>{
+    createNewClient: async (nome, cpf) => {
+        try {
+            const [result] = await db.execute(
+                'INSERT INTO Cliente (nome, cpf) VALUES (?, ?)',
+                [nome, cpf]
+            );
+
+            return {
+                id: result.insertId,
+                nome,
+                cpf
+            };
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    getAllClients: async () => {
+        try {
+            const [rows] = await db.query('SELECT * FROM Cliente');
+            return rows;
+
+        } catch (error) {
+            throw error;
+        }
+
+    },
+    upadteClient: async(id, nome, cpf)=>{
         try{
+            if(!id){
+                throw new Error('ID do cliente é necessário!');
+            }
+const [result] = await db.execute('UPDATE Cliente SET nome = ?, cpf = ? WHWRW id = ?', [nome, cpf, id]);
 
-            const [result] = await db.execute('INSERT INTO Cliente (nome, cpf) VALUES(?, ?)', [nome,]
+        if(result.affectedRows === 0){
+            throw new Error('Cliente não cadastrado!')
+        }
 
-            )
+        }catch (error){
+            throw error;
+        }
+    },
+
+    deleteClient: async (id)=>{
+        try{
+            if(!id){
+                throw new Error('ID do cliente é necessário!');
+            }
+
+            const [ result ] = await db.execute('DELETE FROM Cliente WHERE id = ?', [id]);
+
+            if(result.affectedRows === 0){
+                throw new Error ('Cliente não encontrado');
+            }
+            return { message: 'Cliente excluido com sucesso'};
+
+        }catch (error) {
+            throw error;
         }
     }
-}
+};
+module.exports = Cliente;
+
