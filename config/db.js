f888b7f43d597102c8ca5d01b2bd92cd19429d38
Aluno <e023aluno@outlook.com>
2026-09-11 22:09:55 -0300

    Criação do cliente Controller js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: "123456",
-    database: 'mvc'
+    password: "aluno",
+    database: 'mvc',
+    port: 3302
     
 });
 
